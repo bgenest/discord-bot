c2be78b0f6d9e8eee9c4fd8a2d91a551f8fb52aa
Bryan Genest <bgenest96@gmail.com>
2022-11-08 11:29:35 -0500

    Delete alcohol.py

--- games/Libraries/alcohol.py
+++
@@ -1,9 +0,0 @@
-#alcohol.py
-
-alcohols = {
-            "vodka" :{"cosmo": "https://imgur.com/tGKmM0h", 'gimlet': 'https://imgur.com/4yOK0F0','martini':'https://imgur.com/Wy4B9TL','moscow mule':'https://imgur.com/vHnBtLb',},
-            "whiskey":{'jack and coke':'https://imgur.com/V8ZNAyW', 'whiskey sour': 'https://imgur.com/zcpSk6k','old fashioned':'https://imgur.com/Buaawr2',},
-            "rum":{'dark and stormy':"https://imgur.com/caE1Ixy", 'jungle bird': 'https://imgur.com/LcuD2MV','mai tai':'https://imgur.com/WZYYjVp',},
-            "tequila":{'magarita':"https://imgur.com/FRVoVjS", 'tequila shot!': 'https://imgur.com/PYSqiyR',},
-            "beer":{'guiness':"https://imgur.com/Hd9j6R8"},
- }
